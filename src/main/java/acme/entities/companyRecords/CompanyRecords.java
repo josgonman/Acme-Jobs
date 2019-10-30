0d0c2d45f6cb7559d2726e03c2449f91cc94916b
Pedro Padilla <pedropadillamolina@gmail.com>
2019-10-30 18:51:20 +0100

    30/10 entidad creada

--- src/main/java/acme/entities/companyRecords/CompanyRecords.java
+++ src/main/java/acme/entities/companyRecords/CompanyRecords.java
@@ -18,39 +18,36 @@ import lombok.Setter;
 @Setter
 public class CompanyRecords extends DomainEntity {
 
-	private static final long											serialVersionUID	= 1L;
+	private static final long	serialVersionUID	= 1L;
 
 	@NotBlank
-	private String														name;
+	private String				name;
 
 	@NotBlank
-	private String														sector;
+	private String				sector;
 
 	@NotBlank
-	private String														ceo;
+	private String				ceo;
 
 	@NotBlank
-	private String														activities;
+	private String				activities;
 
 	@NotBlank
 	@URL
-	private String														link;
+	private String				link;
 
 	@NotBlank
-	@Pattern(regexp = "+" + @Range(min = 0, max = 3))
+	@Pattern(regexp = "+" + "\\d\\d\\d " + "(\\d\\d\\d\\d)" + "\\d\\d\\d\\d\\d\\d")
 	private Integer				phone;
 
 	@NotBlank
 	@Email
-	private String														email;
+	private String				email;
 
-
-	private enum incorporated {
-		Inc, LLC
-	};
-
+	@NotBlank
+	private Boolean				incorporated;
 
 	@NotBlank
 	@Range(min = 0, max = 5)
-	private Integer stars;
+	private Integer				stars;
 }
