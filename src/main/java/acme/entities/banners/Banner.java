86fb64bbe773b55493372b6cd557cbb5988ba0ac
Alejandro Fuentes Gómez <alefuegom@alum.us.es>
2019-11-04 11:23:06 +0100

    4/11 Alejandro
    Intentando Banner

---
+++ src/main/java/acme/entities/banners/Banner.java
@@ -0,0 +1,48 @@
+package acme.entities.banners;
+
+import java.awt.Image;
+
+import javax.persistence.Entity;
+
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Banner extends DomainEntity {
+
+
+		private static final long	serialVersionUID	= 1L;
+
+	
+		private Image				picture;
+
+	
+		private String				slogan;
+
+		@URL
+		@NotBlank
+		private String				url;
+
+		
+		private Boolean				comercial;
+		
+		
+		@CreditCardNumber
+		private String 				card;
+		
+		
+		private	String				jingle;
+	
+
+
+	}
+
+
