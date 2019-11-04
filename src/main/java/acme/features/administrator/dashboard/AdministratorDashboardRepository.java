54dc4e68735dd64a1d75303c2e874dbe5ea0548a
Jose Manuel <josgonman@alum.us.es>
2019-11-04 20:10:08 +0100

    Dashboard (listing parameters) no charts funcionando

---
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
@@ -0,0 +1,48 @@
+
+package acme.features.administrator.dashboard;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AdministratorDashboardRepository extends AbstractRepository {
+
+	@Query("select count(a) from Announcement a")
+	Integer getAllAnnouncements();
+
+	@Query("select count(c) from CompanyRecord c")
+	Integer getAllCompanies();
+
+	@Query("select count(i) from InvestorRecord i")
+	Integer getAllInvestor();
+
+	@Query("select min(r.reward.amount) from ActiveRequest r where datediff(current_date(), r.deadline)<0")
+	Double getMinReward();
+
+	@Query("select max(r.reward.amount) from ActiveRequest r where datediff(current_date(), r.deadline)<0")
+	Double getMaxReward();
+
+	@Query("select avg(r.reward.amount) from ActiveRequest r where datediff(current_date(), r.deadline)<0")
+	Double getAvgReward();
+
+	@Query("select stddev(r.reward.amount) from ActiveRequest r where datediff(current_date(), r.deadline)<0")
+	Double getStandDev();
+
+	@Query("select min(o.minMon.amount) from Offer o where datediff(current_date(), o.deadline)<0")
+	Double getMinMoney();
+
+	@Query("select max(o.maxMon.amount) from Offer o where datediff(current_date(), o.deadline)<0")
+	Double getMaxMoney();
+
+	@Query("select avg(o.minMon.amount) from Offer o where datediff(current_date(), o.deadline)<0")
+	Double getAvgMinMoney();
+
+	@Query("select avg(o.maxMon.amount) from Offer o where datediff(current_date(), o.deadline)<0")
+	Double getAvgMaxMoney();
+
+	@Query("select stddev(o.minMon.amount + o.maxMon.amount) from Offer o where datediff(current_date(), o.deadline)<0")
+	Double getStandDevOffer();
+
+}
--- src/main/webapp/META-INF/views/master/menu.jsp
+++ src/main/webapp/META-INF/views/master/menu.jsp
@@ -75,6 +75,8 @@
 			<acme:menu-separator/>
 			<acme:menu-suboption code="master.menu.administrator.announcement" action="/administrator/announcement/list"/>
 			<acme:menu-separator/>
+			<acme:menu-suboption code="master.menu.administrator.dashboard" action="/administrator/dashboard/show"/>
+			<acme:menu-separator/>
 			<acme:menu-suboption code="master.menu.administrator.shutdown" action="/master/shutdown"/>
 		</acme:menu-option>
 
